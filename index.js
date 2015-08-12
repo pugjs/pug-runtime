766bd47e6e8677dc0fe3f39a9127a00e48990f55
Andreas Lubbe <git@lubbe.org>
2015-08-11 21:27:59 -0700

    Remove old code
    Originally from #9.

--- index.js
+++ index.js
@@ -179,17 +179,7 @@ function jade_attrs(obj, terse){
  * @api private
  */
 
-var jade_encode_html_rules = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;'
-};
 var jade_match_html = /[&<>"]/;
-/* istanbul ignore next */
-function jade_encode_char(c) {
-  return jade_encode_html_rules[c] || c;
-}
 exports.escape = jade_escape;
 function jade_escape(_html){
   var html = '' + _html;

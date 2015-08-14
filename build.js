9e4f31e1204d37355331f661070dc73767717e64
Timothy Gu <timothygu99@gmail.com>
2015-08-13 20:18:40 -0700

    Do not allow building only internals

--- build.js
+++ build.js
@@ -2,9 +2,10 @@
 
 var fs = require('fs');
 var dependencies = require('./lib/dependencies.js');
+var internals = require('./lib/internals.js');
 var source = {};
 fs.readdirSync(__dirname + '/lib').forEach(function (filename) {
-  if (filename !== 'dependencies.js') {
+  if (filename !== 'dependencies.js' && filename !== 'internals.js') {
     source[filename.replace(/\.js$/, '')] = fs.readFileSync(__dirname + '/lib/' + filename, 'utf8');
   }
 });
@@ -13,6 +14,9 @@ module.exports = build;
 
 function build(functions) {
   var fns = [];
+  functions = functions.filter(function (fn) {
+    return !internals[fn];
+  });
   for (var i = 0; i < functions.length; i++) {
     if (fns.indexOf(functions[i]) === -1) {
       fns.push(functions[i]);
